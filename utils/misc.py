9f09061e18db45464da5cb6a925530f16356cae6
W1l1z <119625804+W1l1z@users.noreply.github.com>
2024-05-12 16:07:17 +0300

    misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -30,7 +30,7 @@ except git.exc.InvalidGitRepositoryError:
         "origin", "https://github.com/W1l1z/W1l1z-Userbot"
     )
     origin.fetch()
-    repo.create_head("master", origin.refs.master)
+    repo.create_head("main", origin.refs.master)
     repo.heads.master.set_tracking_branch(origin.refs.master)
     repo.heads.master.checkout(True)
     gitrepo = git.Repo(".")
